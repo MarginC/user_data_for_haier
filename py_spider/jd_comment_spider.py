03f4c0daf1dc41f119a383ea5a3e415e5d9628c8
Zhou Yangchao <1028519445@qq.com>
2017-02-14 01:45:45 +0800

    完成基础版本爬虫（参数列表为商品id，支持商品id上并发） 后续工作： 1、抓取商品列表及其价格； 2、根据商品列表进行评论爬取； 3、支持更细粒度的并发； 4、数据文件合并（生成直接可用数据）； 5、支持输出xlsx格式；

--- py_spider/jd_comment_spider.py
+++ py_spider/jd_comment_spider.py
@@ -1,33 +1,24 @@
 # -*- coding:UTF-8 -*-
 
-import re
+import sys
+import os
+import threading
 import urllib.request
 import json
-
-
-# isbn = '7513300712'
-# url = 'http://openisbn.com/isbn/{0}/'.format(isbn)
-# category_pattern = re.compile(r'Category: *.*, ')
-# html = urllib.request.urlopen(url).read()
-# category_info = category_pattern.findall(html.decode(encoding='utf-8'))
-#
-# if len(category_info) > 0:
-#     print(category_info[0])
-# else:
-#     print('get category failed.')
+import time
 
 
 def gen_url(productId, page, pageSize=10, score=0):
     domain = 'http://club.jd.com/comment/productPageComments.action'
     params = 'productId={0}&score={1}&sortType=5&page={2}&pageSize={3}'.format(productId, score, page, pageSize)
-    _url = domain + "?" + params
+    _url = domain + '?' + params
     return _url
 
 
 def get_json(productId, page, pageSize, score):
     _url = gen_url(productId, page, pageSize, score)
-    _html = urllib.request.urlopen(_url).read().decode(encoding='gb2312')
-    _json = json.loads(_html)
+    _html = urllib.request.urlopen(_url).read().decode(encoding='gbk', errors='strict')
+    _json = json.loads(_html, encoding='gbk')
     return _json
 
 
@@ -36,16 +27,94 @@ def get_summary(productId):
     return _json['productCommentSummary']
 
 
+def get_evaluation(score):
+    if score > 3:
+        return '好评'
+    elif score > 1:
+        return '中评'
+    else:
+        return '差评'
+
+
 def get_comments(productId, page):
     _json = get_json(productId, page, 10, 0)
     return _json['comments']
 
 
-comments = get_comments(1773994, 0)
+def write_headers(_file, _headers):
+    for header in _headers[:-1]:
+        print(header, end=',', file=_file)
+    print(_headers[-1], file=_file)
 
-print("nickname,userProvince,userLevelName,userClientShow,isMobile")
-for i in range(0, len(comments)):
-    comment = comments[i]
-    print('{0},{1},{2},{3},{4}'
-          .format(comment['nickname'], comment['userProvince'], comment['userLevelName']
-                  , comment['userClientShow'], comment['isMobile']))
+
+def write_comments(_file, _headers, _comments):
+    for comment in _comments:
+        comment['evaluation'] = get_evaluation(comment['score'])
+        for i in range(len(_headers) - 1):
+            print('"{0}"'.format(comment[_headers[i]]), end=',', file=_file)
+        print('"{0}"'.format(comment[_headers[-1]]), file=_file)
+
+
+def write_file(referenceId):
+    file_name = referenceId + '.csv'
+    if os.path.exists(file_name):
+        return
+    count = 0
+    with open(file_name, mode='w') as file:
+        headers = ('nickname', 'userProvince', 'userLevelName', 'userClientShow', 'isMobile', 'score',
+                   'evaluation', 'creationTime', 'referenceTime', 'days', 'referenceId', 'content')
+        write_headers(file, headers)
+
+        page = 0
+        while True:
+            try:
+                comments = get_comments(referenceId, page)
+                if len(comments) > 0:
+                    write_comments(file, headers, comments)
+                    count += len(comments)
+                else:
+                    break
+            except Exception as e:
+                print(e)
+            page += 1
+    return count
+
+
+def get_time():
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+
+class JDThread(threading.Thread):
+    def __init__(self, referenceId):
+        threading.Thread.__init__(self)
+        self.referenceId = referenceId
+        self.count = 0
+
+    def run(self):
+        self.count = write_file(self.referenceId)
+
+    def get_result(self):
+        return self.count
+
+
+def main():
+    referenceIds = sys.argv[1:]
+    threads = {}
+    print('main process start at {0}'.format(get_time()))
+    for referenceId in referenceIds:
+        threads[referenceId] = JDThread(referenceId)
+        threads[referenceId].start()
+        print('{0} start at {1}'.format(referenceId, get_time()))
+    while len(referenceIds):
+        for referenceId in referenceIds:
+            threads[referenceId].join(5)
+            if not threads[referenceId].isAlive():
+                referenceIds = [i for i in referenceIds if i != referenceId]
+                print('{0} crawled {1} comments, stop at {2}'.
+                      format(referenceId, threads[referenceId].get_result(), get_time()))
+                break;
+    print('main process finish at {0}'.format(get_time()))
+
+
+if __name__ == '__main__':
+    main()
